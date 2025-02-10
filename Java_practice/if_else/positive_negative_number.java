12a57125b9d0e47f91ef2f8432af8c3695a7f582
rasikaG23 <117646149+rasikaG23@users.noreply.github.com>
2025-02-10 13:20:01 +0530

    INITIAL CODE

---
+++ Java_practice/if_else/positive_negative_number.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        System.out.println("Enter a Number");
+        int n=sc.nextInt();
+        
+        if(n==0){
+            System.out.println("Its a neutral number");
+            
+        }
+        
+        else if(n>0){
+            System.out.println("Its a positive number");
+            
+        }else {
+            System.out.println("Its a negative number");
+            
+        }
+    }
+}
